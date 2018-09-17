c773cf12edb7a68df05d08d93f347775227dae7a
Linus <mario0man@gmail.com>
2018-09-17 01:17:44 -0500

    test

--- assignment2/multiagent/multiAgents.py
+++ assignment2/multiagent/multiAgents.py
@@ -160,35 +160,6 @@ class MinimaxAgent(MultiAgentSearchAgent):
             Returns the total number of agents in the game
         """
         "*** YOUR CODE HERE ***"
-        
-
-<<<<<<< HEAD
-=======
-    def maxFunction(self,gameState,depth):
-        if depth==0 or gameState.isWin() or gameState.isLose():
-          return self.evaluationFunction(gameState), "noMove"
-
-        moves=gameState.getLegalActions()
-        scores = [self.minFunction(gameState.generateSuccessor(self.index,move),1, depth) for move in moves]
-        bestScore=max(scores)
-        bestIndices = [index for index in range(len(scores)) if scores[index] == bestScore]
-        chosenIndex = bestIndices[0]
-        return bestScore,moves[chosenIndex]
-
-    def minFunction(self,gameState,agent, depth):
-        if depth==0 or gameState.isWin() or gameState.isLose():
-          return self.evaluationFunction(gameState), "noMove"
-        moves=gameState.getLegalActions(agent) #get legal actions.
-        scores=[]
-        if(agent!=gameState.getNumAgents()-1):
-          scores =[self.minFunction(gameState.generateSuccessor(agent,move),agent+1,depth) for move in moves]
-        else:
-          scores =[self.maxFunction(gameState.generateSuccessor(agent,move),(depth-1))[0] for move in moves]
-        minScore=min(scores)
-        worstIndices = [index for index in range(len(scores)) if scores[index] == minScore]
-        chosenIndex = worstIndices[0]
-        return minScore, moves[chosenIndex]
->>>>>>> origin/ege
 
 class AlphaBetaAgent(MultiAgentSearchAgent):
     """
